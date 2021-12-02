64a0a03b82be2856ad8ce1a0cdd48c732a0c7f70
Curtis Silva <curtissilva77@gmail.com>
2021-12-01 18:52:43 -0700

    member variables/methods for PropertyController

--- Controller/PropertyController.java
+++ Controller/PropertyController.java
@@ -1,5 +1,30 @@
 package Controller;
 
+import java.util.ArrayList;
+
+import Database.Database;
+import GUI.EditPropetyView;
+
 public class PropertyController {
-    
+    //Member variables for class PropertyController
+    private EditPropetyView edit;
+    private ArrayList<Property> listing;
+    private LandlordController landlord;
+    private ManagerController manager;
+    private Database db;
+
+    public void editProperty(Property p)
+    {
+
+    }
+
+    public void removeProperty(Property p)
+    {
+
+    }
+
+    public void updateListing(Property p)
+    {
+
+    }
 }
