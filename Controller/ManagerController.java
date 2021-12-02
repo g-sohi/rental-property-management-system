6721b8aa36027da1fb6b24f3c2a7d9c722604193
Curtis Silva <curtissilva77@gmail.com>
2021-12-01 18:44:02 -0700

    methods and member varaibles for ManagerController

--- Controller/ManagerController.java
+++ Controller/ManagerController.java
@@ -1,5 +1,37 @@
 package Controller;
 
 public class ManagerController {
-    
+    private Manager manager;
+    private ManagerView managerv;
+    private SummaryReportView report;
+    private ViewDataPage viewInfo;
+    private PropertyController prop;
+    private PaymentController pay;
+    private Report reportR;
+    private Database db;
+
+    public void update()
+    {
+
+    }
+
+    public void retrieveReport()
+    {
+
+    }
+
+    public void retrieveRenter()
+    {
+
+    }
+
+    public void retrieveLandlord()
+    {
+
+    }
+
+    public void retrieveProperties()
+    {
+
+    }
 }
