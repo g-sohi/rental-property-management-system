4f3fd44db3d9d87e43e422aa7508f86256bf91d1
ivanezekielsuyat <ivan.suyat@ucalgary.ca>
2021-12-01 18:57:50 -0700

    added UML Attribs+Funcs

--- Controller/RegisterController.java
+++ Controller/RegisterController.java
@@ -1,5 +1,22 @@
 package Controller;
 
+import Database.*;
+import GUI.*;
+import Models.*;
+
 public class RegisterController {
     
+    private CreatePropertyView createProp;
+    private ArrayList<Property> listings;
+    private Database db;
+    private LandlordController landLord;
+
+    public RegisterController() {
+        
+    }
+
+    public void add(Property p) {
+
+    }
+
 }
