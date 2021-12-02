bb7875648faed64b4a5cfd7fb35b9d644ea7763d
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-01 18:48:48 -0700

    update

--- Models/Fees.java
+++ Models/Fees.java
@@ -6,7 +6,54 @@ public class Fees {
     private String feeStartDate;
     private String feeEndDate;
 
-    public Fees(){
-        
+    /**
+     * Constructor for the Fees class
+     */
+    public Fees(double amount,int feePeriod,String feeStartDate,String feeEndDate)
+    {
+        this.amount =amount;
+        this.feePeriod = feePeriod;
+        this.feeStartDate = feeStartDate;
+        this.feeEndDate =feeEndDate;
+    }
+
+    public double getAmount()              //getter method for the amount
+    {
+        return this.amount;
+    }
+
+    public void setAmount(double amount)  //setter method for the amount
+    {
+        this.amount = amount;
+    }
+
+    public int getFeePeriod()              //getter method for the feePeriod
+    {
+        return feePeriod;
+    }
+
+    public void setFeePeriod(int feePeriod)  //setter method for the feePeriod
+    {
+        this.feePeriod = feePeriod;
+    }
+
+    public String getFeeStartDate()              //getter method for the feeStartDate
+    {
+        return feeStartDate;
+    }
+
+    public void setFeeStartDate(String feeStartDate)  //setter method for the feeStartDate
+    {
+        this.feeStartDate = feeStartDate;
+    }
+
+    public String getFeeEndDate()              //getter method for the feeEndDate
+    {
+        return feeEndDate;
+    }
+
+    public void setFeeEndDate(String feeEndDate)  //setter method for the feeEndDate
+    {
+        this.feeEndDate = feeEndDate;
     }
 }
