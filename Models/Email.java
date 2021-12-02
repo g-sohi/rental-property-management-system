0540ef059bdbf29073fa748ec9eb3e32f556f870
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-01 18:27:10 -0700

    update

--- Models/Email.java
+++ Models/Email.java
@@ -6,7 +6,52 @@ public class Email {
     private String recip;
     private String date;
 
-    public Email(){
-
+    public Email(String subject, String message, String recip, String date)
+    {
+        this.subject = subject;
+        this.message = message;
+        this.recip = recip;
+        this.date = date;
     }
+
+    public String getSubject()
+    {
+        return this.subject;
+    }
+
+    public void setSubject(String subject)
+    {
+        this.subject = subject;
+    }
+
+    public String getMessage()
+    {
+        return this.message;
+    }
+
+    public void setMessage(String message)
+    {
+        this.message = message;
+    }
+    
+    public String getRecip()
+    {
+        return this.recip;
+    }
+
+    public void setRecip(String recip)
+    {
+        this.recip = recip;
+    }
+
+    public String getDate()
+    {
+        return this.date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
 }
