3a5048909db71e61a5ea672c44abbc5d3035a35a
Curtis Silva <curtissilva77@gmail.com>
2021-12-01 19:05:58 -0700

    member variables for EditPropertyView class

--- GUI/EditPropetyView.java
+++ GUI/EditPropetyView.java
@@ -1,5 +1,12 @@
 package GUI;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+
 public class EditPropetyView {
-    
+    private JComboBox status;
+    private JButton save;
+    private JButton remove;
+    private JTextField idProperty;
 }
