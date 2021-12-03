b60b38f2de0c354c306faba94a7f7afd6591af05
ivanezekielsuyat <ivan.suyat@ucalgary.ca>
2021-12-02 17:09:37 -0700

    added UML Attribs + Funcs

--- Controller/PaymentController.java
+++ Controller/PaymentController.java
@@ -1,5 +1,29 @@
 package Controller;
 
+import Models.*;
+import GUI.*;
+import Database.*;
+
+import java.util.*;
+
 public class PaymentController {
     
+    private FeesView fees;
+    private Fees fee;
+    private Database db;
+    private LandlordController landlord;
+    private ManagerController manager;
+
+    public SearchController() {
+
+    }
+
+    public void updateFees(int fee) {
+        
+    }
+
+    public void payFees() {
+        
+    }
+
 }
