4eeb248160cb68fcb42051a4eedb78999dd03405
ivanezekielsuyat <ivan.suyat@ucalgary.ca>
2021-12-02 18:04:47 -0700

    added UML attributes

--- GUI/FeesView.java
+++ GUI/FeesView.java
@@ -1,5 +1,9 @@
 package GUI;
 
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
 public class FeesView {
-    
+    private JTextField changeFeeAmount, changePeriod, payFee, creditCard;
+    private JButton submit, payFees;
 }
--- GUI/ManagerView.java
+++ GUI/ManagerView.java
@@ -1,5 +1,7 @@
 package GUI;
 
+import javax.swing.JButton;
+
 public class ManagerView {
-    
+    private JButton generateReport, editFees, updateProperty, viewInformation, search;
 }
