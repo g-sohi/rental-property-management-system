2518f34e706b98948b73b66bf10b3197ece892be
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-02 17:55:05 -0700

    update

--- Models/Property.java
+++ Models/Property.java
@@ -2,7 +2,7 @@ package Models;
 
 public class Property {
     private int id;
-    private String address;
+    private Address address;
     private String type;
     private int numOfBedrooms;
     private int numOfBathrooms;
@@ -10,7 +10,102 @@ public class Property {
     private Fees propertyFees;
     private String propertyStatus;
 
-    public Property(){
-
+    
+    /**
+     * Constructor for the Property class
+     */
+    public Property(int id, Address address,String type, int numOfBedrooms, int numOfBathrooms, boolean furnished, Fees propertyFees, String propertyStatus)
+    {
+        this.id =id;
+        this.address = address;
+        this.type = type;
+        this.numOfBathrooms = numOfBathrooms;
+        this.numOfBedrooms = numOfBedrooms;
+        this.furnished = furnished;
+        this.propertyFees = propertyFees;
+        this.propertyStatus = propertyStatus;
     }
+
+    public int getID()                          //getter method for the id    
+    {
+        return this.id;
+    }
+
+    public void setID(int id)                   //setter method for the id
+    {
+        this.id = id;
+    }
+
+    public Address getAddress()                 //getter method for the address  
+    {
+        return this.address;
+    }
+
+    public void setAddress(Address address)     //setter method for the address
+    {
+        this.address = address;
+    }
+
+    public String getType()                     //getter method for the type  
+    {
+        return this.type;
+    }
+
+    public void setType(String type)            //setter method for the type
+    {   
+        this.type = type;
+    }
+
+    public int getNumOfBedrooms()               //getter method for the numOfBedrooms  
+    {
+        return this.numOfBedrooms;
+    }
+
+    public void setNumOfBedrooms(int numOfBedrooms) //setter method for the numOfBedrooms
+    {
+        this.numOfBedrooms = numOfBedrooms;
+    }
+
+    public int getNumOfBathrooms()              //getter method for the numOfBathrooms  
+    {
+        return this.numOfBathrooms;
+    }
+
+    public void setNumOfBathrooms(int numOfBathrooms)   //setter method for the numOfBathrooms
+    {
+        this.numOfBathrooms = numOfBathrooms;
+    }
+
+    public boolean getFurnished()               //getter method for the furnished  
+    {
+        return this.furnished;
+    }
+
+    public void setFurnished(boolean furnished) //setter method for the furnished
+    {
+        this.furnished = furnished;
+    }
+
+    
+    public Fees getPropertyFees()               //getter method for the propertyFees  
+    {
+        return this.propertyFees;
+    }
+
+    public void setPropertyFees(Fees propertyFees)  //setter method for the propertyFees
+    {
+        this.propertyFees = propertyFees;
+    }
+
+    public String getPropertyStatus()           //getter method for the propertyStatus  
+    {
+        return this.propertyStatus;
+    }
+
+    public void setPropertyStatus(String propertyStatus)    //setter method for the propertyStatus
+    {
+        this.propertyStatus = propertyStatus;
+    }
+
+
 }
