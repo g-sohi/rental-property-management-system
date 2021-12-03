74aeae17dae91a5a66a9fe1060a9bd6f795f108f
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-02 17:11:14 -0700

    update

--- Models/Landlord.java
+++ Models/Landlord.java
@@ -4,12 +4,32 @@ import java.util.ArrayList;
 public class Landlord extends User{
     private Email email;
     private ArrayList<Property> properties;
-
-    public Landlord(){
-
+    
+    /**
+     * Constructor for the Landlord class
+     */
+    public Landlord(Email email){
+        this.email = email;
+        this.properties = new ArrayList<Property>();
     }
 
-    public void createLandlord(){
+    public Email getEmail()              //getter method for the email  
+    {
+        return email;
+    }
 
+    public void setEmail(Email email)      //setter method for the email
+    {
+        this.email = email; 
+    }
+
+    public void addProperty(Property p)         //add a property
+    {
+        properties.add(p);
+    }
+
+    public void removeProperty(Property p)   //remove a property
+    {
+        properties.remove(p);
     }
 }
