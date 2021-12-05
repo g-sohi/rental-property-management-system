49eae8cb1aee530d4b4da10e8008c4a71d1345c3
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-04 18:34:24 -0700

    update

--- Database/Database.java
+++ Database/Database.java
@@ -1,6 +1,6 @@
 package Database;
 
-import GUI.LoginView;
+import Controller.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -70,8 +70,9 @@ public class Database {
 
         public static void main(String[] args) {
             Database db = new Database();
-            LoginView view = new LoginView();
-            db.initializeConnection();
+            LoginController s = new LoginController();
+            s.setUserInformation();
+            //db.initializeConnection();
             //db.addUser();
             db.close();
         }
