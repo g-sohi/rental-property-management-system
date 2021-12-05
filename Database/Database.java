5df00f176714f62f45c703eaae4319a6f9525a16
Gurpartap Sohi <gpsohi@hotmail.com>
2021-12-05 13:30:31 -0700

    added functionality to retrive landlord properties from database

--- Database/Database.java
+++ Database/Database.java
@@ -1,10 +1,10 @@
 package Database;
 
 import Controller.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import Models.*;
+
 import java.sql.*;
+import java.util.*;
 
 public class Database {
 
@@ -18,7 +18,7 @@ public class Database {
 
         public Database() {
             String username = "root";
-            String password = "singh123";
+            String password = "";
     
             try {
 
@@ -75,6 +75,42 @@ public class Database {
             }
         }
 
+        public ArrayList<Property> getLandlordProperties(int landLordID) {
+            ArrayList<Property> properties = new ArrayList<Property>();
+            try {
+                String query = String.format("SELECT * FROM property WHERE Landlord_ID = ?");
+                PreparedStatement stmt = dbConnect.prepareStatement(query);
+    
+                stmt.setInt(1, landLordID);
+    
+                line = stmt.executeQuery();
+                while (line.next()) {
+                    int ID = line.getInt("Property_ID");
+                    String address = line.getString("Address");
+                    String type = line.getString("Type");
+                    int numOfBedrooms = line.getInt("NoOfBedrooms");
+                    int numOfBathrooms = line.getInt("NoOfBathrooms");
+                    String furnished = line.getString("Furnished");
+                    boolean isFurnished = false;
+                    if(furnished.equalsIgnoreCase("yes")){
+                        isFurnished = true;
+                    }
+                    String status = line.getString("Status");
+
+                    Property prop = new Property(ID, address, type, numOfBedrooms, numOfBathrooms, isFurnished, new Fees(50.00, 0, "null", "null"), status);
+                    properties.add(prop);
+                }
+                stmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Error.Exiting program.");
+                System.exit(1);
+            }
+            return properties;
+        }
+
+
+
         public void close() {
             try {
                 this.dbConnect.close();
@@ -86,7 +122,9 @@ public class Database {
 
         public static void main(String[] args) {
             Database db = new Database();
-            LoginController s = new LoginController(db);
+            int id = 3;
+            Landlord land = new Landlord("Robin", "Robin", "Sio", id, "ensf480", "Manager", new Email("null", "null", "null", "null"), db.getLandlordProperties(id));
+            System.out.println("Address: " + land.getProperties().get(0).getAddress() + "\nType: " + land.getProperties().get(0).getType());
             //db.addUser();
         }
 }
