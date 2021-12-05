f531628aa8d1b4c71863bec7e0741072ac8f9678
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-05 12:15:26 -0700

    update

--- GUI/LandlordView.java
+++ GUI/LandlordView.java
@@ -1,7 +1,40 @@
 package GUI;
 
-import javax.swing.JButton;
+
+
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.event.*;
 
 public class LandlordView {
     private JButton registerProperty, updateProperty, payFee, search; //change UML; registerProperties to registerProperty
+    private JFrame f;
+    public LandlordView()
+    {
+        f=  new JFrame("LandLord Page");
+        f.setSize(500, 500);
+        registerProperty = new JButton("RegisterProperty");
+        updateProperty = new JButton("UpdateProperty");
+        payFee = new JButton("PayFee");
+        search = new JButton("Search");
+        f.add(registerProperty);
+        f.add(updateProperty);
+        f.add(payFee);
+        f.add(search);
+        registerProperty.setBounds(30,100,150,30);
+        updateProperty.setBounds(300,100,150,30);
+        payFee.setBounds(50,170,100,30);
+        search.setBounds(330,170,100,30);
+
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public static void main(String [] args)
+    {
+        new LandlordView();
+    }
 }
+
