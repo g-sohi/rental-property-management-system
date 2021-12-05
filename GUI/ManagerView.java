e977ebcf230161d9a6abc780e43bc0147934e1e2
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-05 12:33:51 -0700

    update

--- GUI/ManagerView.java
+++ GUI/ManagerView.java
@@ -1,7 +1,36 @@
 package GUI;
 
-import javax.swing.JButton;
+import javax.swing.*;
 
 public class ManagerView {
     private JButton generateReport, editFees, updateProperty, viewInformation, search;
+    private JFrame f;
+
+    public ManagerView()
+    {
+        f=  new JFrame("Manager Page");
+        f.setSize(500, 500);
+        generateReport = new JButton("Generate Report");
+        editFees = new JButton("Edit Fees");
+        updateProperty = new JButton("Update Status");
+        search = new JButton("Search");
+        viewInformation = new JButton("View Information");
+        f.add(generateReport);
+        f.add(editFees);
+        f.add(updateProperty);
+        f.add(search);
+        f.add(viewInformation);
+        generateReport.setBounds(30,100,150,30);
+        editFees.setBounds(300,100,150,30);
+        updateProperty.setBounds(40,170,130,30);
+        search.setBounds(330,170,100,30);
+        viewInformation.setBounds(180, 220, 150, 30);
+
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+    public static void main(String [] args)
+    {
+        new ManagerView();
+    }
 }
