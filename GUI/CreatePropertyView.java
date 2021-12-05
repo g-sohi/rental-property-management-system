0c0a278e3832d7ab96b810fb40bad54bfb3a821f
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-05 13:32:36 -0700

    update

--- GUI/CreatePropertyView.java
+++ GUI/CreatePropertyView.java
@@ -1,12 +1,14 @@
 package GUI;
 
+import javax.swing.*;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JTextField;
-
-public class CreatePropertyView {
-    private String types[];
-    private String quadrants[];
+import java.awt.*;
+import javax.swing.JComboBox;
+public class CreatePropertyView extends JFrame {
+    private String types[] = {"Detached", "Semi-Detached", "TownHouse", "Condo"};
+    private String quadrants[] = {"NE", "NW", "SE", "SW"};
+    private String fur[] = {"Yes", "No"};
     private JComboBox type;
     private JComboBox quadrant;
     private JTextField noBeds;
@@ -18,4 +20,25 @@ public class CreatePropertyView {
     private JComboBox furnished;
     private JTextField landlordName;
     private JButton register;
+
+    public CreatePropertyView()
+    {
+        setSize(500, 500);
+        getContentPane().setBackground(new Color(200, 200, 250));
+        setTitle("Create New Property");
+        type = new JComboBox<>(types);
+        quadrant = new JComboBox<>(quadrants);
+        noBaths = new JTextField();
+        noBeds = new JTextField();
+        streetNo = new JTextField();
+        streetName = new JTextField();
+        city = new JTextField();
+        postalCode = new JTextField();
+        furnished = new JComboBox<>(fur);
+        landlordName = new JTextField();
+        register = new JButton();
+
+
+
+    }
 }
