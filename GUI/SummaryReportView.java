94957dbfe4959367694f327ad4b5fa3f4e5d97a9
ivanezekielsuyat <ivan.suyat@ucalgary.ca>
2021-12-06 21:57:21 -0700

    set up GUI Components

--- GUI/SummaryReportView.java
+++ GUI/SummaryReportView.java
@@ -3,7 +3,34 @@ package GUI;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 
-public class SummaryReportView {
+public class SummaryReportView extends JFrame {
+    
+    // variable declaration
     private JFrame frame;
     private JButton close;
+
+    public SummaryReportView() {
+        
+        // creation of JFrame of certain size
+        frame = new JFrame("Summary Report");
+        frame.setSize(500, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        frame.setLayout(null); // layout set to null
+
+        // creation of JButton of certain size and positioon
+        close = new JButton("Close");
+        close.setBounds(200, 400, 100, 50);
+
+        frame.add(close);
+
+        frame.setVisible(true);
+
+    }
+
+    // **for testing purposes**
+    public static void main(String [] args) {
+        new SummaryReportView();
+    }
+
 }
