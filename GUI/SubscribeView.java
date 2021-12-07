58aebf9cb5920e928010eafd9c6d18e9e18a7b74
ivanezekielsuyat <ivan.suyat@ucalgary.ca>
2021-12-07 14:22:17 -0700

    added GUI components

--- GUI/SubscribeView.java
+++ GUI/SubscribeView.java
@@ -1,9 +1,57 @@
 package GUI;
 
-import javax.swing.JComboBox;
-import javax.swing.JButton;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class SubscribeView {
+public class SubscribeView extends JFrame {
+    
+    // variable declaration
     private JComboBox subscribe;
-    private JButton submit, clearNotify;
+    private String subStatus[] = {"Subscribed", "Unsubscribed"};
+    private JButton submit, clearNotify; //change UML; remove submit?
+
+    public SubscribeView() {
+
+        // creation of JFrame of certain size
+        JFrame f = new JFrame("Notifications Page");
+        f.setSize(500, 500);
+        f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        f.setLayout(null);
+
+        // creation of JLabels of certain size and position
+        JLabel notifLabel = new JLabel("Notifications:");
+        notifLabel.setBounds(50, 25, 100, 40);
+        JLabel subStatLabel = new JLabel("Subscription Status:");
+        subStatLabel.setBounds(50, 350, 150, 40);        
+        
+        // creation of JTextArea of certain size and position
+        JTextArea notifs = new JTextArea();
+        notifs.setBounds(50, 60, 400, 240);
+        notifs.setEditable(false);
+
+        // creation of JComboBox of certain size and position
+        subscribe = new JComboBox(subStatus);
+        subscribe.setBounds(175, 360, 275, 20);
+
+        // creation of JButtons of certain size and position
+        JButton back = new JButton("Back");
+        back.setBounds(275, 410, 150, 40);
+        clearNotify = new JButton("Clear Notifications");
+        clearNotify.setBounds(75, 410, 150, 40);
+
+        // addition of components to JFrame
+        f.add(notifLabel); f.add(notifs);
+        f.add(subStatLabel); f.add(subscribe);
+        f.add(back);
+        f.add(clearNotify);
+
+        f.setVisible(true); // JFrame is set to appear
+
+    }
+
+    // **for testing purposes**
+    // public static void main(String [] args) {
+    //     new SubscribeView();
+    // }
+
 }
