fa401cc0904b0b0bc9e9b21290efae8e6f5f1cda
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-08 17:22:14 -0700

    update

--- Controller/GUIController.java
+++ Controller/GUIController.java
@@ -13,15 +13,19 @@ private MainPageGUI mainpage;
 private LoginController login;
 private SearchController search;
 private Database db;
-
+private RegisterView rView;
 public GUIController(Database db) throws IOException
 {
     mainpage = new MainPageGUI();
     this.setDatabase(db);
     login = new LoginController(this.db);
     search = new SearchController(this.db);
+    rView = new RegisterView();
     this.mainpage.addLoginListener(this);
     this.mainpage.addGuestListener(this);
+    this.mainpage.addRegisterListener(this);
+    this.rView.addRegisterListener(this);
+
 }
 
 public void setDatabase(Database db)
@@ -44,16 +48,31 @@ public void actionPerformed(ActionEvent e) {
         search.enableView();
         search.getView().turnOn();
     }
+    else if(e.getSource().equals(mainpage.getRegister()))
+    {
+        mainpage.setOff();
+        rView.turnOn();
+        this.rView.addRegisterListener(this);
+    }
+     if(e.getSource().equals(rView.getRegisterButton()))   
+        {
+            System.out.print("not vsiisble");
+            rView.showDialog();
+            rView.destroyFrame();
+            mainpage.turnOn();
+        }
+    
+    
     if(login.getView() != null)
     {
      if(e.getSource().equals(login.getView().getGoBackButton()))   
     {
+        System.out.print(" vsiisble");
         login.getView().destroyFrame();
         mainpage.turnOn();
-
     }
-
-    
 }
+
+
 }
 }
