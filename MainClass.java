334162f203096e81bc49c0e57e720b3920ae8de4
manpreet singh <manpreet.singh2@ucalgary.ca>
2021-12-10 14:37:54 -0700

    update

--- MainClass.java
+++ MainClass.java
@@ -6,6 +6,13 @@ import java.io.IOException;
 import Controller.*;
 public class MainClass {
 
+    /**
+     *  MainClass run the program. It initailises
+     * the Database and GUI controller
+     * and set the Database in the GUIController object ctrl
+     * */ 
+
+
     public static void main(String[] args) throws IOException {
         Database db = new Database();
         GUIController ctrl = new GUIController(db);
